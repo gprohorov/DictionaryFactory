c87423d3a9c348404189eb6303c6bc700b68c1ca
george <yuramail222@gmail.com>
2017-07-23 21:58:29 +0300

    Final commit

--- src/main/java/DictionaryFactory.java
+++ src/main/java/DictionaryFactory.java
@@ -5,12 +5,12 @@ import java.util.stream.Collectors;
  * Created by george on 20.07.17.
  */
 public class DictionaryFactory {
-   // private String str;
+
     private Map<Character,ArrayList<String>> dictionary;
     private final Comparator<String> comparator;
 
     public DictionaryFactory() {
-        //this.str = str;
+
         this.comparator = (String a, String b) ->{
             if (a.length()!=b.length()) {
                 return b.length() - a.length();
@@ -28,9 +28,13 @@ public class DictionaryFactory {
         Character firstChar;
         ArrayList<String> values;
         this.dictionary.clear();
+
+        // get array of words from input string
         if (s.equals("")) return this.dictionary;
         String[] array = s.split(" ");
 
+        // get a map of the words,
+        // words are sorted according to comparator
         for (String word:array){
             firstChar = word.charAt(0);
             values = new ArrayList<>();
@@ -41,17 +45,7 @@ public class DictionaryFactory {
             Collections.sort(values,comparator);
             dictionary.put(firstChar,values);
         }
-/*        System.out.println(dictionary.toString());
-        System.out.println("-------------");*/
-        Map<Character,List<String>> map = new TreeMap<>();
-           map =     Arrays.stream(s.split("n"))
-                .collect(Collectors.groupingBy(word->word.charAt(0)))
-               .entrySet().stream()
-                .filter(el->el.getValue().size()>1)
-                 //  .map(el->el.)
-                .collect(Collectors
-                .toMap(e -> e.getKey(),e -> e.getValue()))
-                ;
+        // filter off small entries
         return dictionary.entrySet().stream()
                 .filter(el->el.getValue().size()>1)
                 .collect(Collectors
